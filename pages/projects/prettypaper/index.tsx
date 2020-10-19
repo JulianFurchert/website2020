a6ad114213a9078ee4c423ee6db5036f26675995
Julian Furchert <julian.furchert@raytion.com>
2020-10-19 17:39:09 +0200

    Fix

--- pages/projects/prettypaper/index.tsx
+++ pages/projects/prettypaper/index.tsx
@@ -4,15 +4,17 @@ import { Text, Container } from '../../../components'
 
 export default function Home() {
   return (
-    <Container>
-      <Text variant="headline" css={{marginBottom: 20}}>
-        Pretty Paper
-      </Text>
-      <Text variant="bodyLarge">
-        Hi, I’m Julian Furchert, a designer and developer based in Düsseldorf. Specializing in user interface design and typography. Furthermore, I‘m working on experimental tools and custom design applications.
-      </Text>
-    </Container>
-    <Container size="large">
-    </Container>
+    <React.Fragment>
+      <Container>
+        <Text variant="headline" css={{marginBottom: 20}}>
+          Pretty Paper
+        </Text>
+        <Text variant="bodyLarge">
+          Hi, I’m Julian Furchert, a designer and developer based in Düsseldorf. Specializing in user interface design and typography. Furthermore, I‘m working on experimental tools and custom design applications.
+        </Text>
+      </Container>
+      <Container size="large">
+      </Container>
+    </React.Fragment>
   )
 }
